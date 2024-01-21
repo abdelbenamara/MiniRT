c615f285c16ee2efff5b0e232c1543842e819723
Abdel Benamara <abdelb.contact@gmail.com>
2024-01-22 00:46:28 +0100

    Updated ft_shape_add

--- ft_shape_add.c
+++ ft_shape_add.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 22:08:10 by abenamar          #+#    #+#             */
-/*   Updated: 2024/01/20 17:13:21 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/01/21 20:19:57 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ static uint8_t	ft_sphere_add(t_scene *scene, char **info)
 	new = ft_lstnew(sphere);
 	if (!sphere || !new)
 		return (ft_pstderr(__ERR_2), free(sphere), free(new), 0);
-	u = ft_vec3_init(info[1]);
+	u = ft_vec3_new(info[1]);
 	if (!u)
 		return (ft_lstdelone(new, free), 0);
 	sphere->center = *u;
@@ -34,7 +34,7 @@ static uint8_t	ft_sphere_add(t_scene *scene, char **info)
 		return (ft_pstderr(__ERR_5), ft_lstdelone(new, free), 0);
 	if (sphere->diameter < 0.0)
 		return (ft_pstderr(__ERR_13), ft_lstdelone(new, free), 0);
-	sphere->color = ft_color_init(info[3]);
+	sphere->color = ft_color_value(info[3]);
 	if (sphere->color == -1)
 		return (ft_lstdelone(new, free), 0);
 	ft_lstadd_front(&scene->spheres, new);
@@ -53,18 +53,18 @@ static uint8_t	ft_plane_add(t_scene *scene, char **info)
 	new = ft_lstnew(plane);
 	if (!plane || !new)
 		return (ft_pstderr(__ERR_2), free(plane), free(new), 0);
-	u = ft_vec3_init(info[1]);
+	u = ft_vec3_new(info[1]);
 	if (!u)
 		return (ft_lstdelone(new, free), 0);
 	plane->point = *u;
-	(free(u), u = ft_vec3_init(info[2]));
+	(free(u), u = ft_vec3_new(info[2]));
 	if (!u)
 		return (ft_lstdelone(new, free), 0);
 	plane->orientation = *u;
 	if (u->x < -1.0 || u->y < -1.0 || u->z < -1.0
 		|| u->x > 1.0 || u->y > 1.0 || u->z > 1.0)
 		return (ft_pstderr(__ERR_10), free(u), ft_lstdelone(new, free), 0);
-	(free(u), plane->color = ft_color_init(info[3]));
+	(free(u), plane->color = ft_color_value(info[3]));
 	if (plane->color == -1)
 		return (ft_lstdelone(new, free), 0);
 	return (ft_lstadd_front(&scene->planes, new), 1);
@@ -82,18 +82,18 @@ static uint8_t	ft_cylinder_add(t_scene *scene, char **info)
 	new = ft_lstnew(cylinder);
 	if (!cylinder || !new)
 		return (ft_pstderr(__ERR_2), free(cylinder), free(new), 0);
-	u = ft_vec3_init(info[1]);
+	u = ft_vec3_new(info[1]);
 	if (!u)
 		return (ft_lstdelone(new, free), 0);
 	cylinder->center = *u;
-	(free(u), u = ft_vec3_init(info[2]));
+	(free(u), u = ft_vec3_new(info[2]));
 	if (!u)
 		return (ft_lstdelone(new, free), 0);
 	cylinder->orientation = *u;
 	if (u->x < -1.0 || u->y < -1.0 || u->z < -1.0
 		|| u->x > 1.0 || u->y > 1.0 || u->z > 1.0)
 		return (ft_pstderr(__ERR_10), free(u), ft_lstdelone(new, free), 0);
-	(free(u), cylinder->color = ft_color_init(info[5]));
+	(free(u), cylinder->color = ft_color_value(info[5]));
 	if (cylinder->color == -1)
 		return (ft_lstdelone(new, free), 0);
 	return (ft_lstadd_front(&scene->cylinders, new), 1);
@@ -101,16 +101,17 @@ static uint8_t	ft_cylinder_add(t_scene *scene, char **info)
 
 uint8_t	ft_shape_add(t_scene *scene, char **info)
 {
-	const int	n = !ft_strncmp(info[0], "sp", 3) \
-					+ 2 * !ft_strncmp(info[0], "pl", 3) \
-					+ 3 * !ft_strncmp(info[0], "cy", 3);
-	t_cylinder	*cylinder;
+	static uint8_t	(*ft_item_add[])(t_scene *, char **) = {
+		ft_sphere_add, ft_plane_add, ft_cylinder_add
+	};
+	const int		n = !ft_strncmp(info[0], "sp", 3) \
+						+ 2 * !ft_strncmp(info[0], "pl", 3) \
+						+ 3 * !ft_strncmp(info[0], "cy", 3);
+	t_cylinder		*cylinder;
 
 	if (!n)
 		return (ft_pstderr(__ERR_12), 0);
-	if ((n == 1 && !ft_sphere_add(scene, info))
-		|| (n == 2 && !ft_plane_add(scene, info))
-		|| (n == 3 && !ft_cylinder_add(scene, info)))
+	if (!ft_item_add[n - 1](scene, info))
 		return (0);
 	if (n == 3)
 	{
