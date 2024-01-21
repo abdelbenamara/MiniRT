9ba7fc893343ed103c9e286a03aaf1ff5f45d529
Abdel Benamara <abdelb.contact@gmail.com>
2024-01-22 00:47:57 +0100

    Updated ft_pixel_put

--- ft_pixel_put.c
+++ ft_pixel_put.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 18:19:40 by abenamar          #+#    #+#             */
-/*   Updated: 2023/12/28 18:20:00 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/01/21 18:18:38 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_pixel_put(t_ximage ximage, int x, int y, int color)
 {
 	char	*pos;
 
-	if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT)
+	if (x < 0 || x >= __WIDTH || y < 0 || y >= __HEIGHT)
 		return ;
 	pos = ximage.data + x * (ximage.bpp / 8) + y * ximage.lsize;
 	*((unsigned int *) pos) = mlx_get_color_value(ximage.mlx, color);
