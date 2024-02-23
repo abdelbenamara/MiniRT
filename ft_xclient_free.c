cc8d81a1f5ccde3e566e5f78f1562824cb758087
Abdel Benamara <abdelb.contact@gmail.com>
2024-02-24 00:56:30 +0100

    Updated ft_xclient_free

--- ft_xclient_free.c
+++ ft_xclient_free.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 18:21:34 by abenamar          #+#    #+#             */
-/*   Updated: 2024/01/21 19:07:45 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/02/04 09:35:49 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 void	ft_xclient_free(t_xclient *xclient)
 {
 	ft_scene_free(xclient->scene);
+	if (xclient->img)
+		mlx_destroy_image(xclient->mlx, xclient->img);
 	if (xclient->win)
 		mlx_destroy_window(xclient->mlx, xclient->win);
 	if (xclient->mlx)
