39414e69dd31d900dc7f742656906c5dc3075b3a
Abdel Benamara <abdelb.contact@gmail.com>
2024-02-24 00:55:32 +0100

    Updated ft_light_add

--- ft_light_add.c
+++ ft_light_add.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 12:48:21 by abenamar          #+#    #+#             */
-/*   Updated: 2024/01/21 20:19:57 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/02/08 23:42:19 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,8 +34,8 @@ uint8_t	ft_light_add(t_scene *scene, char **info)
 		return (ft_pstderr(__ERR_5), ft_lstdelone(new, free), 0);
 	if (light->bratio < 0.0 || light->bratio > 1.0)
 		return (ft_pstderr(__ERR_6), ft_lstdelone(new, free), 0);
-	light->color = ft_color_value(info[3]);
-	if (light->color == -1)
+	light->color = ft_color_read(info[3]);
+	if (isnan(light->color.x))
 		return (ft_lstdelone(new, free), 0);
 	ft_lstadd_front(&scene->lights, new);
 	return (1);
