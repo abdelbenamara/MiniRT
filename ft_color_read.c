f68c9f85a51b110f3cc44e71a51061e9dcf718b7
Abdel Benamara <abdelb.contact@gmail.com>
2024-02-24 00:54:41 +0100

    Updated ft_color_read

--- ft_color_read.c
+++ ft_color_read.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_color_value.c                                   :+:      :+:    :+:   */
+/*   ft_color_read.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/30 22:11:12 by abenamar          #+#    #+#             */
-/*   Updated: 2024/01/21 20:20:21 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/02/08 23:42:19 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-int	ft_color_value(const char *str)
+t_vec3	ft_color_read(const char *str)
 {
 	char	*s1;
 	char	*s2;
@@ -25,11 +25,11 @@ int	ft_color_value(const char *str)
 	if (s1)
 		s2 = ft_strchr(s1 + 1, ',');
 	if (!s2 || ft_strchr(s2 + 1, ','))
-		return (ft_pstderr(__ERR_7), -1);
+		return (ft_pstderr(__ERR_7), ft_vec3(nan(""), nan(""), nan("")));
 	r = ft_atoi(str);
 	g = ft_atoi(s1 + 1);
 	b = ft_atoi(s2 + 1);
 	if (r < 0 || g < 0 || b < 0 || r > 255 || g > 255 || b > 255)
-		return (ft_pstderr(__ERR_8), -1);
-	return (r << 16 | g << 8 | b);
+		return (ft_pstderr(__ERR_8), ft_vec3(nan(""), nan(""), nan("")));
+	return (ft_vec3((1.0 * r) / 255.0, (1.0 * g) / 255.0, (1.0 * b) / 255.0));
 }
