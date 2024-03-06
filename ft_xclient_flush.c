685b171b0a93083b7580c15cd28ed427b153c349
Abdel Benamara <abdelb.contact@gmail.com>
2024-03-06 23:40:43 +0100

    Updated ft_xclient_flush

--- ft_xclient_flush.c
+++ ft_xclient_flush.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 09:50:56 by abenamar          #+#    #+#             */
-/*   Updated: 2024/02/04 17:52:10 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/02/29 01:16:19 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,4 @@ void	ft_xclient_flush(t_xclient *xclient)
 	xclient->img = xclient->buf;
 	if (tmp)
 		mlx_destroy_image(xclient->mlx, tmp);
-	xclient->update = 1;
 }
