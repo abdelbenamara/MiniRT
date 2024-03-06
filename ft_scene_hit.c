ad53305d1d70508182c22fce4e52f131ce384963
Abdel Benamara <abdelb.contact@gmail.com>
2024-03-06 23:40:28 +0100

    Updated ft_scene_hit

--- ft_scene_hit.c
+++ ft_scene_hit.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/07 00:48:52 by abenamar          #+#    #+#             */
-/*   Updated: 2024/02/22 23:06:42 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/03/06 23:05:15 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,31 @@ static uint8_t	ft_sphere_hit(t_sphere *s, t_ray r, t_interval i, t_hit *h)
 		return (0);
 	sqrt_d = sqrt(d);
 	root = (-abc.y - sqrt_d) / abc.x;
-	if (root <= i.min || root >= i.max)
+	if (i.min >= root || root >= i.max)
 	{
 		root = (-abc.y + sqrt_d) / abc.x;
-		if (root <= i.min || root >= i.max)
+		if (i.min >= root || root >= i.max)
 			return (0);
 	}
 	h->t = root;
 	h->point = ft_vec3_sum(r.origin, ft_vec3_prod(r.direction, h->t));
 	h->color = s->color;
+	return (1);
+}
+
+static uint8_t	ft_plane_hit(t_plane *p, t_ray r, t_interval i, t_hit *h)
+{
+	const double	denom = ft_vec3_dot(r.direction, p->orientation);
+	double			t;
+
+	if (fabs(denom) < 1.0E-8)
+		return (0);
+	t = ft_vec3_dot(ft_vec3_diff(p->point, r.origin), p->orientation) / denom;
+	if (i.min > t || t > i.max)
+		return (0);
+	h->t = t;
+	h->point = ft_vec3_sum(r.origin, ft_vec3_prod(r.direction, h->t));
+	h->color = p->color;
 	return (1);
 }
 
@@ -50,6 +66,13 @@ t_color	ft_scene_hit(t_scene *scene, t_ray r, t_interval i)
 	while (lst)
 	{
 		if (ft_sphere_hit(lst->content, r, i, &h))
+			i.max = h.t;
+		lst = lst->next;
+	}
+	lst = scene->planes;
+	while (lst)
+	{
+		if (ft_plane_hit(lst->content, r, i, &h))
 			i.max = h.t;
 		lst = lst->next;
 	}
