f250c88531263a81493e93db76227f73f59bd9e9
Abdel Benamara <abdelb.contact@gmail.com>
2024-03-14 17:24:35 +0100

    Updated ft_pixel_color

--- src/render/ft_pixel_color.c
+++ src/render/ft_pixel_color.c
@@ -1,32 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_color_build.c                                   :+:      :+:    :+:   */
+/*   ft_pixel_color.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 19:05:15 by abenamar          #+#    #+#             */
-/*   Updated: 2024/02/09 10:12:35 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/03/14 15:14:44 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-static double	ft_interval_clamp(t_interval i, double d)
+static float	ft_clamp(float d, float min, float max)
 {
-	if (d < i.min)
-		return (i.min);
-	if (d > i.max)
-		return (i.max);
+	if (d < min)
+		return (min);
+	if (d > max)
+		return (max);
 	return (d);
 }
 
-int	ft_color_build(t_color color, int spp)
+int	ft_pixel_color(t_color color, int spp)
 {
-	const t_interval	i = ft_interval(0.000, 0.999);
-	const int			r = 256 * ft_interval_clamp(i, sqrt(color.x / spp));
-	const int			g = 256 * ft_interval_clamp(i, sqrt(color.y / spp));
-	const int			b = 256 * ft_interval_clamp(i, sqrt(color.z / spp));
+	const float	scale = 1.0F / spp;
+	const int	r = 256 * ft_clamp(sqrtf(color.x * scale), 0.000F, 0.999F);
+	const int	g = 256 * ft_clamp(sqrtf(color.y * scale), 0.000F, 0.999F);
+	const int	b = 256 * ft_clamp(sqrtf(color.z * scale), 0.000F, 0.999F);
 
 	return (r << 16 | g << 8 | b);
 }
