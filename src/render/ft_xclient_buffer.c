01070ac16d86c5015474e34f2ec8cb374de98195
Abdel Benamara <abdelb.contact@gmail.com>
2024-03-19 00:49:47 +0100

    Updated ft_xclient_buffer

--- src/render/ft_xclient_buffer.c
+++ src/render/ft_xclient_buffer.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 09:20:35 by abenamar          #+#    #+#             */
-/*   Updated: 2024/02/08 00:15:41 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/03/16 14:02:39 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void	ft_xclient_buffer(t_xclient *xclient)
 {
-	xclient->mlx = xclient->mlx;
 	xclient->buf = mlx_new_image(xclient->mlx, _WIDTH, _HEIGHT);
 	if (!xclient->buf)
 		(ft_xclient_free(xclient), exit(EXIT_FAILURE));
