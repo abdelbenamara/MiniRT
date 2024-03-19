67839340ab333688f6fc5a628bc177f01fbac322
Abdel Benamara <abdelb.contact@gmail.com>
2024-03-19 01:13:05 +0100

    Updated ft_plane_add

--- src/scene/ft_plane_add.c
+++ src/scene/ft_plane_add.c
@@ -6,34 +6,34 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 15:35:10 by abenamar          #+#    #+#             */
-/*   Updated: 2024/03/14 16:26:29 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/03/17 16:15:55 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-uint8_t	ft_plane_add(t_scene *scene, char **info)
+bool	ft_plane_add(t_scene *scene, char **info)
 {
-	t_plane	*plane;
+	t_plane	*pl;
 	t_list	*new;
 
 	if (ft_tab_size(info) != 4)
-		return (ft_pstderr(__ERR_4), 0);
-	plane = malloc(sizeof(t_plane));
-	new = ft_lstnew(plane);
-	if (!plane || !new)
-		return (ft_pstderr(__ERR_2), free(plane), free(new), 0);
-	plane->point = ft_vec3_read(info[1]);
-	if (isnanf(plane->point.x))
-		return (ft_lstdelone(new, free), 0);
-	plane->normal = ft_vec3_read(info[2]);
-	if (isnanf(plane->normal.x) || !ft_vec3_is_normalized(plane->normal))
-		return (ft_lstdelone(new, free), 0);
-	plane->normal = ft_vec3_unit(plane->normal);
-	plane->color = ft_color_read(info[3]);
-	if (isnanf(plane->color.x))
-		return (ft_lstdelone(new, free), 0);
-	plane->theta = ft_vec3(0.0F, 0.0F, 0.0F);
+		return (ft_pstderr(__ERR_4), false);
+	pl = malloc(sizeof(t_plane));
+	new = ft_lstnew(pl);
+	if (!pl || !new)
+		return (ft_pstderr(__ERR_2), free(pl), free(new), false);
+	pl->point = ft_vec3_read(info[1]);
+	if (isnan(pl->point.x))
+		return (ft_lstdelone(new, free), false);
+	pl->normal = ft_vec3_read(info[2]);
+	if (isnan(pl->normal.x) || !ft_vec3_is_normalized(pl->normal))
+		return (ft_lstdelone(new, free), false);
+	pl->color = ft_color_read(info[3]);
+	if (isnan(pl->color.x))
+		return (ft_lstdelone(new, free), false);
+	pl->normal = ft_vec3_unit(pl->normal);
+	pl->theta = ft_vec3(0.0F, 0.0F, 0.0F);
 	ft_lstadd_front(&scene->planes, new);
-	return (1);
+	return (true);
 }
