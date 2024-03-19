5284d416c299303f79cca949fd87ae241ca5a8ee
Abdel Benamara <abdelb.contact@gmail.com>
2024-03-19 01:13:16 +0100

    Updated ft_scene_new

--- src/scene/ft_scene_new.c
+++ src/scene/ft_scene_new.c
@@ -6,24 +6,24 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 11:00:24 by abenamar          #+#    #+#             */
-/*   Updated: 2024/03/14 16:40:52 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/03/16 13:33:08 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-static uint8_t	ft_element_setup(t_scene *scene, char **info)
+static bool	ft_element_setup(t_scene *scene, char **info)
 {
-	static uint8_t	(*ft_setup[])(t_scene *, char **) = {
+	static bool	(*ft_setup[])(t_scene *, char **) = {
 		ft_ambiance_init, ft_camera_init, ft_light_add,
 		ft_sphere_add, ft_plane_add, ft_cylinder_add
 	};
-	int				id;
+	int			id;
 
 	if (!info)
-		return (0);
+		return (false);
 	if (!info[0] || info[0][0] == '#')
-		return (1);
+		return (true);
 	id = !ft_strncmp(info[0], "A", 2) \
 		+ 2 * !ft_strncmp(info[0], "C", 2) \
 		+ 3 * !ft_strncmp(info[0], "L", 2) \
@@ -31,13 +31,13 @@ static uint8_t	ft_element_setup(t_scene *scene, char **info)
 		+ 5 * !ft_strncmp(info[0], "pl", 3) \
 		+ 6 * !ft_strncmp(info[0], "cy", 3);
 	if (!id)
-		return (ft_pstderr(__ERR_12), 0);
+		return (ft_pstderr(__ERR_12), false);
 	if (!ft_setup[id - 1](scene, info))
-		return (0);
-	return (1);
+		return (false);
+	return (true);
 }
 
-static uint8_t	ft_scene_setup(t_scene *scene, const int fd)
+static bool	ft_scene_setup(t_scene *scene, const int fd)
 {
 	char	*line;
 	char	**info;
@@ -52,23 +52,23 @@ static uint8_t	ft_scene_setup(t_scene *scene, const int fd)
 			while (line)
 				(free(line), line = get_next_line(fd));
 			if (!info)
-				return (ft_pstderr(__ERR_2), 0);
-			return (ft_tab_free(info), 0);
+				return (ft_pstderr(__ERR_2), false);
+			return (ft_tab_free(info), false);
 		}
 		free(line);
 		line = get_next_line(fd);
 		ft_tab_free(info);
 	}
 	if (scene->camera == NULL)
-		return (ft_pstderr(__ERR_14), 0);
-	return (1);
+		return (ft_pstderr(__ERR_14), false);
+	return (true);
 }
 
 t_scene	*ft_scene_new(const char *file)
 {
 	t_scene	*scene;
 	int		fd;
-	uint8_t	ready;
+	bool	ready;
 
 	scene = malloc(sizeof(t_scene));
 	if (!scene)
