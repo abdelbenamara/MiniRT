9f3e672d4e06c16e191f276bcdc3ffbe5ccdbe50
Abdel Benamara <abdelb.contact@gmail.com>
2024-03-19 01:16:00 +0100

    Updated ft_pixel_put

--- src/render/ft_pixel_put.c
+++ src/render/ft_pixel_put.c
@@ -6,18 +6,44 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 18:19:40 by abenamar          #+#    #+#             */
-/*   Updated: 2024/02/08 00:15:41 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/03/19 00:50:45 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-void	ft_pixel_put(t_xclient *xclient, int x, int y, int color)
+static float	ft_clamp(float d, float min, float max)
+{
+	if (d < min)
+		return (min);
+	if (d > max)
+		return (max);
+	return (d);
+}
+
+static int	ft_color_rgb(t_color color)
+{
+	float	scale;
+	int		r;
+	int		g;
+	int		b;
+
+	scale = 1.0F;
+	if (_SAMPLES_PER_PIXEL > 0)
+		scale = 1.0F / _SAMPLES_PER_PIXEL;
+	r = 256 * ft_clamp(sqrtf(color.x * scale), 0.000F, 0.999F);
+	g = 256 * ft_clamp(sqrtf(color.y * scale), 0.000F, 0.999F);
+	b = 256 * ft_clamp(sqrtf(color.z * scale), 0.000F, 0.999F);
+	return (r << 16 | g << 8 | b);
+}
+
+void	ft_pixel_put(t_xclient *xclient, int x, int y, t_color color)
 {
 	char	*pos;
 
 	if (x < 0 || x >= _WIDTH || y < 0 || y >= _HEIGHT)
 		return ;
 	pos = xclient->data + x * (xclient->bpp / 8) + y * xclient->lsize;
-	*((unsigned int *) pos) = mlx_get_color_value(xclient->mlx, color);
+	*((unsigned int *) pos) = mlx_get_color_value(\
+		xclient->mlx, ft_color_rgb(color));
 }
