67276ebec10bf49e8ad72f2a7718e03d88bb6fc7
Abdel Benamara <abdelb.contact@gmail.com>
2024-03-19 01:12:57 +0100

    Updated ft_light_add

--- src/scene/ft_light_add.c
+++ src/scene/ft_light_add.c
@@ -6,34 +6,34 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 12:48:21 by abenamar          #+#    #+#             */
-/*   Updated: 2024/03/14 16:20:25 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/03/16 13:31:35 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-uint8_t	ft_light_add(t_scene *scene, char **info)
+bool	ft_light_add(t_scene *scene, char **info)
 {
 	t_light	*light;
 	t_list	*new;
 
 	if (ft_tab_size(info) != 4)
-		return (ft_pstderr(__ERR_4), 0);
+		return (ft_pstderr(__ERR_4), false);
 	light = malloc(sizeof(t_light));
 	new = ft_lstnew(light);
 	if (!light || !new)
-		return (ft_pstderr(__ERR_2), free(light), free(new), 0);
+		return (ft_pstderr(__ERR_2), free(light), free(new), false);
 	light->position = ft_vec3_read(info[1]);
-	if (isnanf(light->position.x))
-		return (ft_lstdelone(new, free), 0);
+	if (isnan(light->position.x))
+		return (ft_lstdelone(new, free), false);
 	light->brightness = ft_atof(info[2]);
-	if (isnanf(light->brightness))
-		return (ft_pstderr(__ERR_5), ft_lstdelone(new, free), 0);
+	if (isnan(light->brightness))
+		return (ft_pstderr(__ERR_5), ft_lstdelone(new, free), false);
 	if (light->brightness < 0.0F || light->brightness > 1.0F)
-		return (ft_pstderr(__ERR_6), ft_lstdelone(new, free), 0);
+		return (ft_pstderr(__ERR_6), ft_lstdelone(new, free), false);
 	light->color = ft_color_read(info[3]);
-	if (isnanf(light->color.x))
-		return (ft_lstdelone(new, free), 0);
+	if (isnan(light->color.x))
+		return (ft_lstdelone(new, free), false);
 	ft_lstadd_front(&scene->lights, new);
-	return (1);
+	return (true);
 }
