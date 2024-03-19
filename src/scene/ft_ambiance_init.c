b2921073b709fffe4b47759b68690c4a2fc5781e
Abdel Benamara <abdelb.contact@gmail.com>
2024-03-19 01:11:59 +0100

    Updated ft_ambiance_init

--- src/scene/ft_ambiance_init.c
+++ src/scene/ft_ambiance_init.c
@@ -6,28 +6,28 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 14:59:37 by abenamar          #+#    #+#             */
-/*   Updated: 2024/03/14 16:20:15 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/03/16 13:29:04 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-uint8_t	ft_ambiance_init(t_scene *scene, char **info)
+bool	ft_ambiance_init(t_scene *scene, char **info)
 {
 	if (scene->ambiance)
-		return (ft_pstderr(__ERR_3), 0);
+		return (ft_pstderr(__ERR_3), false);
 	if (ft_tab_size(info) != 3)
-		return (ft_pstderr(__ERR_4), 0);
+		return (ft_pstderr(__ERR_4), false);
 	scene->ambiance = malloc(sizeof(t_ambiance));
 	if (!scene->ambiance)
-		return (ft_pstderr(__ERR_2), 0);
+		return (ft_pstderr(__ERR_2), false);
 	scene->ambiance->lighting = ft_atof(info[1]);
-	if (isnanf(scene->ambiance->lighting))
-		return (ft_pstderr(__ERR_5), 0);
+	if (isnan(scene->ambiance->lighting))
+		return (ft_pstderr(__ERR_5), false);
 	if (scene->ambiance->lighting < 0.0F || scene->ambiance->lighting > 1.0F)
-		return (ft_pstderr(__ERR_6), 0);
+		return (ft_pstderr(__ERR_6), false);
 	scene->ambiance->color = ft_color_read(info[2]);
-	if (isnanf(scene->ambiance->color.x))
-		return (0);
-	return (1);
+	if (isnan(scene->ambiance->color.x))
+		return (false);
+	return (true);
 }
