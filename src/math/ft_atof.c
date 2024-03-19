9c096b9db121fda51970de412d35f008ca5c7803
Abdel Benamara <abdelb.contact@gmail.com>
2024-03-19 01:11:27 +0100

    Updated ft_atof

--- src/math/ft_atof.c
+++ src/math/ft_atof.c
@@ -6,20 +6,20 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/30 19:29:05 by abenamar          #+#    #+#             */
-/*   Updated: 2024/03/13 15:40:17 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/03/19 00:33:50 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-static uint8_t	ft_isfloat(const char *nptr)
+static bool	ft_isfloat(const char *nptr)
 {
 	while (*nptr == ' ' || (9 <= *nptr && *nptr <= 13))
 		++nptr;
 	if (*nptr == '+' || *nptr == '-')
 		++nptr;
 	if (!ft_isdigit(*nptr))
-		return (0);
+		return (false);
 	while (ft_isdigit(*nptr))
 		++nptr;
 	if (*nptr == '.')
@@ -27,8 +27,8 @@ static uint8_t	ft_isfloat(const char *nptr)
 	while (ft_isdigit(*nptr))
 		++nptr;
 	if (*nptr)
-		return (0);
-	return (1);
+		return (false);
+	return (true);
 }
 
 static float	ft_intlenf(int n)
@@ -57,16 +57,17 @@ float	ft_atof(const char *nptr)
 {
 	float	f;
 	char	*s;
+	int		i;
 	float	d;
 
 	if (!ft_isfloat(nptr))
-		return (nanf(""));
+		return (NAN);
 	f = ft_atoi(nptr);
 	s = ft_strchr(nptr, '.');
 	if (!s)
 		return (f);
-	d = ft_atoi(s + 1);
-	d /= powf(10.0F, ft_intlenf(d));
+	i = ft_atoi(s + 1);
+	d = (1.0F / powf(10.0F, ft_intlenf(i))) * i;
 	while (*nptr == ' ' || (9 <= *nptr && *nptr <= 13))
 		++nptr;
 	if (*nptr == '-')
