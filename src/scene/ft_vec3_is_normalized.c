2cbc1bed60be0aa5cb20d85b680bee91523dd7de
Abdel Benamara <abdelb.contact@gmail.com>
2024-03-19 01:12:24 +0100

    Updated ft_vec3_is_normalized

--- src/scene/ft_vec3_is_normalized.c
+++ src/scene/ft_vec3_is_normalized.c
@@ -6,16 +6,16 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 14:41:11 by abenamar          #+#    #+#             */
-/*   Updated: 2024/03/14 14:52:03 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/03/16 13:34:05 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-uint8_t	ft_vec3_is_normalized(t_vec3 u)
+bool	ft_vec3_is_normalized(t_vec3 u)
 {
 	if (u.x < -1.0F || u.y < -1.0F || u.z < -1.0F
 		|| u.x > 1.0F || u.y > 1.0F || u.z > 1.0F)
-		return (ft_pstderr(__ERR_10), 0);
-	return (1);
+		return (ft_pstderr(__ERR_10), false);
+	return (true);
 }
