4e1ee672920991a78c7f0b53ecb348f183964dbc
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-01 01:38:22 +0200

    Updated ft_face_normal

--- src/render/ft_face_normal.c
+++ src/render/ft_face_normal.c
@@ -6,15 +6,15 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 16:05:21 by abenamar          #+#    #+#             */
-/*   Updated: 2024/03/18 14:21:34 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/03/24 14:33:12 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-t_vec3	ft_face_normal(t_vec3 direction, t_vec3 normal)
+t_vec3f	ft_face_normal(t_vec3f const direction, t_vec3f const normal)
 {
-	if (ft_vec3_dot(direction, normal) > 0.0F)
-		return (ft_vec3_prod(normal, -1.0F));
+	if (ft_vec3f_dot(direction, normal) > 0.0F)
+		return (ft_vec3f_prod(normal, -1.0F));
 	return (normal);
 }
