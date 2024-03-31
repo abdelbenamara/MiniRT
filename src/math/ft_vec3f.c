f735fa4e018cd8372b3dd5d0750b2a1e4a1ad082
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-01 01:29:14 +0200

    Updated ft_vec3f

--- src/math/ft_vec3f.c
+++ src/math/ft_vec3f.c
@@ -1,20 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_vec3.c                                          :+:      :+:    :+:   */
+/*   ft_vec3f.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 23:31:39 by abenamar          #+#    #+#             */
-/*   Updated: 2024/03/13 11:43:47 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/03/24 14:18:15 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-t_vec3	ft_vec3(float x, float y, float z)
+t_vec3f	ft_vec3f(float const x, float const y, float const z)
 {
-	t_vec3	u;
+	t_vec3f	u;
 
 	u.x = x;
 	u.y = y;
