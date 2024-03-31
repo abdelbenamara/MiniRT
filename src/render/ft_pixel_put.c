39208223a4bd24c560ef3d3b7566e33b01cd823e
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-01 01:40:18 +0200

    Updated ft_pixel_put

--- src/render/ft_pixel_put.c
+++ src/render/ft_pixel_put.c
@@ -6,13 +6,20 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 18:19:40 by abenamar          #+#    #+#             */
-/*   Updated: 2024/03/19 00:50:45 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/03/24 14:36:38 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-static float	ft_clamp(float d, float min, float max)
+static float	ft_scale(int const samples_per_pixel)
+{
+	if (samples_per_pixel > 0)
+		return (1.0F / samples_per_pixel);
+	return (1.0F);
+}
+
+static float	ft_clamp(float const d, float const min, float const max)
 {
 	if (d < min)
 		return (min);
@@ -21,23 +28,21 @@ static float	ft_clamp(float d, float min, float max)
 	return (d);
 }
 
-static int	ft_color_rgb(t_color color)
+static int	ft_color_rgb(t_color3f const color)
 {
-	float	scale;
-	int		r;
-	int		g;
-	int		b;
+	float const	scale = ft_scale(_SAMPLES_PER_PIXEL);
+	int			r;
+	int			g;
+	int			b;
 
-	scale = 1.0F;
-	if (_SAMPLES_PER_PIXEL > 0)
-		scale = 1.0F / _SAMPLES_PER_PIXEL;
 	r = 256 * ft_clamp(sqrtf(color.x * scale), 0.000F, 0.999F);
 	g = 256 * ft_clamp(sqrtf(color.y * scale), 0.000F, 0.999F);
 	b = 256 * ft_clamp(sqrtf(color.z * scale), 0.000F, 0.999F);
 	return (r << 16 | g << 8 | b);
 }
 
-void	ft_pixel_put(t_xclient *xclient, int x, int y, t_color color)
+void	ft_pixel_put(t_xclient const *const xclient, \
+	int const x, int const y, t_color3f const color)
 {
 	char	*pos;
 
