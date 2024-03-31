e2f22abe0a9830bb3e1c36a674d19688f268a76d
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-01 01:34:04 +0200

    Updated ft_pstderr

--- src/utils/ft_pstderr.c
+++ src/utils/ft_pstderr.c
@@ -6,13 +6,13 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/30 19:17:12 by abenamar          #+#    #+#             */
-/*   Updated: 2023/12/30 19:30:21 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/03/24 13:52:54 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-ssize_t	ft_pstderr(const char *str)
+ssize_t	ft_pstderr(char const *str)
 {
 	return (write(STDERR_FILENO, str, ft_strlen(str) * sizeof(char)));
 }
