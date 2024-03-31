4cdbb787fb5da1b79924da35f00d237f69a83cd9
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-01 01:37:14 +0200

    Updated ft_sphere_add

--- src/scene/ft_sphere_add.c
+++ src/scene/ft_sphere_add.c
@@ -6,13 +6,13 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 15:34:18 by abenamar          #+#    #+#             */
-/*   Updated: 2024/03/17 16:15:47 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/03/30 18:06:27 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-bool	ft_sphere_add(t_scene *scene, char **info)
+bool	ft_sphere_add(t_scene *const scene, char *const *info)
 {
 	t_sphere	*sp;
 	t_list		*new;
@@ -23,19 +23,19 @@ bool	ft_sphere_add(t_scene *scene, char **info)
 	new = ft_lstnew(sp);
 	if (!sp || !new)
 		return (ft_pstderr(__ERR_2), free(sp), free(new), false);
-	sp->center = ft_vec3_read(info[1]);
+	sp->center = ft_vec3f_read(info[1]);
 	if (isnan(sp->center.x))
 		return (ft_lstdelone(new, free), false);
 	sp->radius = ft_atof(info[2]);
 	if (isnan(sp->radius))
 		return (ft_pstderr(__ERR_5), ft_lstdelone(new, free), false);
-	if (sp->radius < 0.0F)
+	if (signbit(sp->radius))
 		return (ft_pstderr(__ERR_13), ft_lstdelone(new, free), false);
-	sp->color = ft_color_read(info[3]);
+	sp->color = ft_color3f_read(info[3]);
 	if (isnan(sp->color.x))
 		return (ft_lstdelone(new, free), false);
 	sp->radius *= 0.5F;
-	sp->radius_squared = powf(sp->radius, 2.0F);
+	sp->radius_squared = sp->radius * sp->radius;
 	sp->radius_reciprocal = 1.0F / sp->radius;
 	ft_lstadd_front(&scene->spheres, new);
 	return (true);
