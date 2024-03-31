203744142576c5be6cf2cfe3b591e5a6535e2327
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-01 01:37:18 +0200

    Updated ft_plane_add

--- src/scene/ft_plane_add.c
+++ src/scene/ft_plane_add.c
@@ -6,13 +6,13 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 15:35:10 by abenamar          #+#    #+#             */
-/*   Updated: 2024/03/17 16:15:55 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/03/30 15:44:08 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-bool	ft_plane_add(t_scene *scene, char **info)
+bool	ft_plane_add(t_scene *const scene, char *const *info)
 {
 	t_plane	*pl;
 	t_list	*new;
@@ -23,17 +23,16 @@ bool	ft_plane_add(t_scene *scene, char **info)
 	new = ft_lstnew(pl);
 	if (!pl || !new)
 		return (ft_pstderr(__ERR_2), free(pl), free(new), false);
-	pl->point = ft_vec3_read(info[1]);
+	pl->point = ft_vec3f_read(info[1]);
 	if (isnan(pl->point.x))
 		return (ft_lstdelone(new, free), false);
-	pl->normal = ft_vec3_read(info[2]);
-	if (isnan(pl->normal.x) || !ft_vec3_is_normalized(pl->normal))
+	pl->normal = ft_vec3f_read(info[2]);
+	if (isnan(pl->normal.x) || !ft_vec3f_isnormalized(pl->normal))
 		return (ft_lstdelone(new, free), false);
-	pl->color = ft_color_read(info[3]);
+	pl->color = ft_color3f_read(info[3]);
 	if (isnan(pl->color.x))
 		return (ft_lstdelone(new, free), false);
-	pl->normal = ft_vec3_unit(pl->normal);
-	pl->theta = ft_vec3(0.0F, 0.0F, 0.0F);
+	pl->normal = ft_vec3f_unit(pl->normal);
 	ft_lstadd_front(&scene->planes, new);
 	return (true);
 }
