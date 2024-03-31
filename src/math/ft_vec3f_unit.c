d428de0d2370cdb8b9b80a939155a7de06ddbed9
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-01 01:30:45 +0200

    Updated ft_vec3f_unit

--- src/math/ft_vec3f_unit.c
+++ src/math/ft_vec3f_unit.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_vec3_unit.c                                     :+:      :+:    :+:   */
+/*   ft_vec3f_unit.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 23:33:55 by abenamar          #+#    #+#             */
-/*   Updated: 2024/03/13 11:46:34 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/03/24 16:27:23 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-t_vec3	ft_vec3_unit(t_vec3 u)
+t_vec3f	ft_vec3f_unit(t_vec3f const u)
 {
-	return (ft_vec3_prod(u, 1.0F / ft_vec3_len(u)));
+	return (ft_vec3f_prod(u, 1.0F / sqrtf(ft_vec3f_dot(u, u))));
 }
