4cd75f9a7920dd4dc7cffd35bb3cc970a2f1ebfc
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-01 01:38:03 +0200

    Updated ft_ray

--- src/render/ft_ray.c
+++ src/render/ft_ray.c
@@ -6,13 +6,13 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 18:55:03 by abenamar          #+#    #+#             */
-/*   Updated: 2024/02/08 23:42:19 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/04/01 01:12:57 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-t_ray	ft_ray(t_point3 origin, t_vec3 direction)
+t_ray	ft_ray(t_point3f const origin, t_vec3f const direction)
 {
 	t_ray	r;
 
