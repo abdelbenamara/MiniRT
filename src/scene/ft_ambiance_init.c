0462040c8403272440f49885516efab53702c388
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-01 01:37:02 +0200

    Updated ft_ambiance_init

--- src/scene/ft_ambiance_init.c
+++ src/scene/ft_ambiance_init.c
@@ -6,13 +6,13 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 14:59:37 by abenamar          #+#    #+#             */
-/*   Updated: 2024/03/16 13:29:04 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/03/24 14:29:30 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-bool	ft_ambiance_init(t_scene *scene, char **info)
+bool	ft_ambiance_init(t_scene *const scene, char *const *info)
 {
 	if (scene->ambiance)
 		return (ft_pstderr(__ERR_3), false);
@@ -24,9 +24,9 @@ bool	ft_ambiance_init(t_scene *scene, char **info)
 	scene->ambiance->lighting = ft_atof(info[1]);
 	if (isnan(scene->ambiance->lighting))
 		return (ft_pstderr(__ERR_5), false);
-	if (scene->ambiance->lighting < 0.0F || scene->ambiance->lighting > 1.0F)
+	if (signbit(scene->ambiance->lighting) || scene->ambiance->lighting > 1.0F)
 		return (ft_pstderr(__ERR_6), false);
-	scene->ambiance->color = ft_color_read(info[2]);
+	scene->ambiance->color = ft_color3f_read(info[2]);
 	if (isnan(scene->ambiance->color.x))
 		return (false);
 	return (true);
