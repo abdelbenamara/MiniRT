f98410d1d13c2ca302d8eccd8a758e18cd24c135
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-01 01:36:55 +0200

    Updated ft_vec3f_isnormalized

--- src/scene/ft_vec3f_isnormalized.c
+++ src/scene/ft_vec3f_isnormalized.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_vec3_is_normalized.c                            :+:      :+:    :+:   */
+/*   ft_vec3f_isnormalized.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 14:41:11 by abenamar          #+#    #+#             */
-/*   Updated: 2024/03/16 13:34:05 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/03/24 14:20:34 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-bool	ft_vec3_is_normalized(t_vec3 u)
+bool	ft_vec3f_isnormalized(t_vec3f const u)
 {
 	if (u.x < -1.0F || u.y < -1.0F || u.z < -1.0F
 		|| u.x > 1.0F || u.y > 1.0F || u.z > 1.0F)
