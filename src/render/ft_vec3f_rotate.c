34f4170492f90c6b0bf19fc0df60a54bf1454355
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-01 01:41:36 +0200

    Updated ft_vec3f_rotate

--- src/render/ft_vec3f_rotate.c
+++ src/render/ft_vec3f_rotate.c
@@ -1,18 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_vec3_len.c                                      :+:      :+:    :+:   */
+/*   ft_vec3f_rotate.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/22 23:11:12 by abenamar          #+#    #+#             */
-/*   Updated: 2024/03/13 12:14:56 by abenamar         ###   ########.fr       */
+/*   Created: 2024/03/30 13:16:09 by abenamar          #+#    #+#             */
+/*   Updated: 2024/04/01 01:14:33 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-float	ft_vec3_len(t_vec3 u)
+t_vec3f	ft_vec3f_rotate(t_vec3f const point, t_quat4f const q)
 {
-	return (sqrtf(ft_vec3_dot(u, u)));
+	return (ft_quat4f_prod(ft_quat4f_prod(q, ft_quat4f(point, 0.0F)), \
+		ft_quat4f(ft_vec3f_prod(q.xyz, -1.0F), q.w)).xyz);
 }
