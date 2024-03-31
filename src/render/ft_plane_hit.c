db6a38a55519c0cd02ec776209a87382f231b856
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-01 01:39:10 +0200

    Updated ft_plane_hit

--- src/render/ft_plane_hit.c
+++ src/render/ft_plane_hit.c
@@ -6,26 +6,26 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 13:46:45 by abenamar          #+#    #+#             */
-/*   Updated: 2024/03/18 14:30:41 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/03/24 14:33:47 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-void	ft_plane_hit(t_plane *pl, t_ray r, t_hit *h)
+void	ft_plane_hit(t_plane *const pl, t_ray const r, t_hit *const h)
 {
-	const float	dn = ft_vec3_dot(r.direction, pl->normal);
+	float const	dn = ft_vec3f_dot(r.direction, pl->normal);
 	float		t;
 
 	if (fabsf(dn) <= FLT_EPSILON)
 		return ;
-	t = ft_vec3_dot(ft_vec3_diff(pl->point, r.origin), pl->normal) / dn;
+	t = ft_vec3f_dot(ft_vec3f_diff(pl->point, r.origin), pl->normal) / dn;
 	if (!isfinite(t) || signbit(t) || h->t <= t)
 		return ;
 	h->t = t;
-	h->point = ft_vec3_sum(r.origin, ft_vec3_prod(r.direction, h->t));
+	h->point = ft_vec3f_sum(r.origin, ft_vec3f_prod(r.direction, h->t));
 	if (dn > 0.0F)
-		h->normal = ft_vec3_prod(pl->normal, -1.0F);
+		h->normal = ft_vec3f_prod(pl->normal, -1.0F);
 	else
 		h->normal = pl->normal;
 	h->color = pl->color;
