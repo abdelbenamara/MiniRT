0f383f68f540e36e534f028108f6d09dd99bc79d
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-03 02:10:54 +0200

    Updated ft_light_add

--- src/scene/ft_light_add.c
+++ src/scene/ft_light_add.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 12:48:21 by abenamar          #+#    #+#             */
-/*   Updated: 2024/03/24 14:29:30 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/04/03 00:27:55 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,15 @@ bool	ft_light_add(t_scene *const scene, char *const *info)
 	new = ft_lstnew(light);
 	if (!light || !new)
 		return (ft_pstderr(__ERR_2), free(light), free(new), false);
-	light->position = ft_vec3f_read(info[1]);
+	light->position = ft_str_to_vec3f(info[1]);
 	if (isnan(light->position.x))
 		return (ft_lstdelone(new, free), false);
-	light->brightness = ft_atof(info[2]);
-	if (isnan(light->brightness))
+	light->brightness = ft_str_to_float(info[2]);
+	if (!isfinite(light->brightness))
 		return (ft_pstderr(__ERR_5), ft_lstdelone(new, free), false);
 	if (signbit(light->brightness) || light->brightness > 1.0F)
 		return (ft_pstderr(__ERR_6), ft_lstdelone(new, free), false);
-	light->color = ft_color3f_read(info[3]);
+	light->color = ft_str_to_color3f(info[3]);
 	if (isnan(light->color.x))
 		return (ft_lstdelone(new, free), false);
 	ft_lstadd_front(&scene->lights, new);
