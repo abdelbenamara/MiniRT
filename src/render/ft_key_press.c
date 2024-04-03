42b1757b079600e9f65a1b36edfacc9131e94200
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-03 02:11:33 +0200

    Updated ft_key_press

--- src/render/ft_key_press.c
+++ src/render/ft_key_press.c
@@ -6,34 +6,43 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 18:31:35 by abenamar          #+#    #+#             */
-/*   Updated: 2024/04/01 01:04:02 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/04/03 02:04:53 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
+static t_basis	ft_basis(t_vec3f const x, t_vec3f const y, t_vec3f const z)
+{
+	t_basis	basis;
+
+	basis.x = x;
+	basis.y = y;
+	basis.z = z;
+	return (basis);
+}
+
 static bool
 	ft_key_translate(int const keycode, \
-		t_vec3f const front, t_point3f *const position)
+		t_camera *const camera, t_point3f *const position)
 {
-	t_vec3f const	left = ft_vec3f_cross(\
-		ft_vec3f(0.0F, 1.0F, 0.0F), front);
-	t_basis const	orthonormal = ft_basis(\
-		left, ft_vec3f_cross(front, left), front);
+	t_basis const	orthogonal = ft_basis(\
+		ft_vec3f_cross(camera->vup, camera->orientation), \
+		camera->vup, camera->orientation);
 	t_vec3f			axis;
 
 	if (keycode == XK_w)
-		axis = ft_vec3f_prod(orthonormal.z, -0.1F);
+		axis = ft_vec3f_prod(orthogonal.z, -0.1F);
 	else if (keycode == XK_s)
-		axis = ft_vec3f_prod(orthonormal.z, 0.1F);
+		axis = ft_vec3f_prod(orthogonal.z, 0.1F);
 	else if (keycode == XK_a)
-		axis = ft_vec3f_prod(orthonormal.x, -0.1F);
+		axis = ft_vec3f_prod(orthogonal.x, -0.1F);
 	else if (keycode == XK_d)
-		axis = ft_vec3f_prod(orthonormal.x, 0.1F);
+		axis = ft_vec3f_prod(orthogonal.x, 0.1F);
 	else if (keycode == XK_q)
-		axis = ft_vec3f_prod(orthonormal.y, -0.1F);
+		axis = ft_vec3f_prod(orthogonal.y, -0.1F);
 	else if (keycode == XK_e)
-		axis = ft_vec3f_prod(orthonormal.y, 0.1F);
+		axis = ft_vec3f_prod(orthogonal.y, 0.1F);
 	else
 		return (false);
 	*position = ft_vec3f_sum(*position, axis);
@@ -71,27 +80,26 @@ static bool
 	return (true);
 }
 
-static bool	ft_camera_handle(int const keycode, t_camera *const camera)
+int	ft_key_press(int const keycode, t_xclient *const xclient)
 {
 	static float const	min_agl = FLT_EPSILON + __M_PIF / 180.0F;
 	static float const	max_agl = __M_PIF - FLT_EPSILON - __M_PIF / 180.0F;
 	static float const	agl = __M_PIF / 180.0F;
 
-	if (keycode == XK_i && camera->fov > min_agl)
-		return (camera->fov -= agl, true);
-	if (keycode == XK_o && camera->fov < max_agl)
-		return (camera->fov += agl, true);
-	if (ft_key_translate(keycode, camera->orientation, &camera->position))
-		return (true);
-	return (ft_key_rotate(\
-		keycode, &camera->rotation, &camera->orientation, &camera->vup));
-}
-
-int	ft_key_press(int const keycode, t_xclient *const xclient)
-{
+	xclient->update = true;
 	if (keycode == XK_Escape)
 		mlx_loop_end(xclient->mlx);
+	else if (keycode == XK_i && xclient->scene->camera->fov > min_agl)
+		xclient->scene->camera->fov -= agl;
+	else if (keycode == XK_o && xclient->scene->camera->fov < max_agl)
+		xclient->scene->camera->fov += agl;
+	else if (ft_key_translate(keycode, xclient->scene->camera, \
+		&xclient->scene->camera->position))
+		;
+	else if (ft_key_rotate(keycode, &xclient->scene->camera->rotation, \
+		&xclient->scene->camera->orientation, &xclient->scene->camera->vup))
+		;
 	else
-		xclient->update = ft_camera_handle(keycode, xclient->scene->camera);
+		xclient->update = false;
 	return (EXIT_SUCCESS);
 }
