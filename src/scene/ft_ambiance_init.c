55d086d0f72438410a98c4f43d4d63ebfd4be16c
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-03 02:10:44 +0200

    Updated ft_ambiance_init

--- src/scene/ft_ambiance_init.c
+++ src/scene/ft_ambiance_init.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 14:59:37 by abenamar          #+#    #+#             */
-/*   Updated: 2024/03/24 14:29:30 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/04/03 00:41:28 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ bool	ft_ambiance_init(t_scene *const scene, char *const *info)
 	scene->ambiance = malloc(sizeof(t_ambiance));
 	if (!scene->ambiance)
 		return (ft_pstderr(__ERR_2), false);
-	scene->ambiance->lighting = ft_atof(info[1]);
-	if (isnan(scene->ambiance->lighting))
+	scene->ambiance->lighting = ft_str_to_float(info[1]);
+	if (!isfinite(scene->ambiance->lighting))
 		return (ft_pstderr(__ERR_5), false);
 	if (signbit(scene->ambiance->lighting) || scene->ambiance->lighting > 1.0F)
 		return (ft_pstderr(__ERR_6), false);
-	scene->ambiance->color = ft_color3f_read(info[2]);
+	scene->ambiance->color = ft_str_to_color3f(info[2]);
 	if (isnan(scene->ambiance->color.x))
 		return (false);
 	return (true);
