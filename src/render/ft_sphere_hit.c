5836b9b0d3dab522990d409422cd16b1040d4db9
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-08 15:03:45 +0200

    Updated ft_sphere_hit

--- src/render/ft_sphere_hit.c
+++ src/render/ft_sphere_hit.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 13:46:16 by abenamar          #+#    #+#             */
-/*   Updated: 2024/04/01 01:14:21 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/04/07 20:32:38 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ void	ft_sphere_hit(t_sphere *const sp, t_ray const r, t_hit *const h)
 {
 	t_vec3f const	oc = ft_vec3f_diff(sp->center, r.origin);
 	float const		nb = ft_vec3f_dot(oc, r.direction);
-	float const		c = ft_vec3f_dot(oc, oc) - sp->radius_squared;
+	float const		c = ft_vec3f_dot(oc, oc) - sp->radius.square;
 	float			d;
 	float			t;
 
 	if (!isfinite(nb) || !isfinite(c) || (signbit(nb) && c >= 0.0F))
 		return ;
-	d = nb * nb - c;
+	d = powf(nb, 2.0F) - c;
 	if (!isfinite(d) || signbit(d))
 		return ;
 	if (c >= 0.0F)
@@ -34,6 +34,6 @@ void	ft_sphere_hit(t_sphere *const sp, t_ray const r, t_hit *const h)
 	h->t = t;
 	h->point = ft_vec3f_sum(r.origin, ft_vec3f_prod(r.direction, h->t));
 	h->normal = ft_face_normal(r.direction, ft_vec3f_prod(\
-		ft_vec3f_diff(h->point, sp->center), sp->radius_reciprocal));
+		ft_vec3f_diff(h->point, sp->center), sp->radius.reciprocal));
 	h->color = sp->color;
 }
