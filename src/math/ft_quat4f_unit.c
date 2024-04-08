74a33a77c127954966dc29db2b31974d9da9ba97
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-08 14:59:26 +0200

    Updated ft_quat4f_unit

--- src/math/ft_quat4f_unit.c
+++ src/math/ft_quat4f_unit.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 16:15:40 by abenamar          #+#    #+#             */
-/*   Updated: 2024/03/30 18:39:20 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/04/04 20:47:09 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 t_quat4f	ft_quat4f_unit(t_quat4f const q)
 {
-	float const	t = 1.0F / sqrtf(q.w * q.w + ft_vec3f_dot(q.xyz, q.xyz));
+	float const	t = 1.0F / sqrtf(powf(q.w, 2.0F) + ft_vec3f_dot(q.xyz, q.xyz));
 
 	return (ft_quat4f(ft_vec3f_prod(q.xyz, t), q.w * t));
 }
