803aaa947e5502d361e77e1553be46fe1a75436e
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-08 15:02:33 +0200

    Updated ft_plane_add

--- src/scene/ft_plane_add.c
+++ src/scene/ft_plane_add.c
@@ -6,11 +6,22 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 15:35:10 by abenamar          #+#    #+#             */
-/*   Updated: 2024/04/02 23:52:34 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/04/04 01:08:01 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
+
+static void	ft_plane_setup(t_plane *pl)
+{
+	t_vec3f const	front = ft_vec3f(0.0F, 0.0F, 1.0F);
+
+	pl->normal = ft_vec3f_unit(pl->normal);
+	pl->rotation.xyz = ft_vec3f_cross(front, pl->normal);
+	pl->rotation.w = sqrtf(1 + ft_vec3f_dot(front, pl->normal));
+	pl->rotation = ft_quat4f_unit(pl->rotation);
+	pl->normal = ft_vec3f_unit(ft_vec3f_rotate(front, pl->rotation));
+}
 
 bool	ft_plane_add(t_scene *const scene, char *const *info)
 {
@@ -32,7 +43,7 @@ bool	ft_plane_add(t_scene *const scene, char *const *info)
 	pl->color = ft_str_to_color3f(info[3]);
 	if (isnan(pl->color.x))
 		return (ft_lstdelone(new, free), false);
-	pl->normal = ft_vec3f_unit(pl->normal);
+	ft_plane_setup(pl);
 	ft_lstadd_front(&scene->planes, new);
 	return (true);
 }
