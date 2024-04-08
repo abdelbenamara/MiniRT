1945af2ff7eb7365bc17456df257c1b74f21ea04
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-08 15:02:30 +0200

    Updated ft_sphere_add

--- src/scene/ft_sphere_add.c
+++ src/scene/ft_sphere_add.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 15:34:18 by abenamar          #+#    #+#             */
-/*   Updated: 2024/04/03 00:29:59 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/04/06 17:57:30 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,17 +26,15 @@ bool	ft_sphere_add(t_scene *const scene, char *const *info)
 	sp->center = ft_str_to_vec3f(info[1]);
 	if (isnan(sp->center.x))
 		return (ft_lstdelone(new, free), false);
-	sp->radius = ft_str_to_float(info[2]);
-	if (!isfinite(sp->radius))
+	sp->radius.twice = ft_str_to_float(info[2]);
+	if (!isfinite(sp->radius.twice))
 		return (ft_pstderr(__ERR_5), ft_lstdelone(new, free), false);
-	if (signbit(sp->radius))
+	if (signbit(sp->radius.twice))
 		return (ft_pstderr(__ERR_14), ft_lstdelone(new, free), false);
 	sp->color = ft_str_to_color3f(info[3]);
 	if (isnan(sp->color.x))
 		return (ft_lstdelone(new, free), false);
-	sp->radius *= 0.5F;
-	sp->radius_squared = sp->radius * sp->radius;
-	sp->radius_reciprocal = 1.0F / sp->radius;
+	sp->radius = ft_radius(sp->radius.twice);
 	ft_lstadd_front(&scene->spheres, new);
 	return (true);
 }
