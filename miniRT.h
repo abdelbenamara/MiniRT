3009991a00b5deaed36d22436fa46eba87d9aacc
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-08 14:59:07 +0200

    Updated miniRT

--- miniRT.h
+++ miniRT.h
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 18:08:16 by abenamar          #+#    #+#             */
-/*   Updated: 2024/04/03 02:04:40 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/04/08 14:54:10 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,8 +63,8 @@ t_quat4f	ft_quat4f_unit(t_quat4f const q);
 
 # define __USAGE	"Usage: miniRT file.rt\n"
 # define __ERR_1	"Error: wrong file extension (not *.rt)\n"
-# define __ERR_2	"Error: out of memory (failed allocation)\n"
-# define __ERR_3	"Error: duplicated element identifier (not unique A or C)\n"
+# define __ERR_2	"Error: out of memory\n"
+# define __ERR_3	"Error: duplicated capital identifier (not unique A,C,L)\n"
 # define __ERR_4	"Error: wrong number of element informations\n"
 # define __ERR_5	"Error: wrong floating-point value (not finite number)\n"
 # define __ERR_6	"Error: invalid lightning ratio (not in range [0,1])\n"
@@ -78,18 +78,53 @@ t_quat4f	ft_quat4f_unit(t_quat4f const q);
 # define __ERR_14	"Error: invalid distance (not greater or equal to 0)\n"
 # define __ERR_15	"Error: missing camera declaration (no 'C' identifier)\n"
 
+typedef struct s_radius
+{
+	float	value;
+	float	twice;
+	float	square;
+	float	reciprocal;
+}	t_radius;
+
+typedef t_vec3f	t_point3f;
+
+typedef struct s_arrow
+{
+	t_point3f	start;
+	t_point3f	center;
+	t_point3f	end;
+	float		height;
+}	t_arrow;
+
+typedef struct s_focus
+{
+	int			type;
+	t_point3f	*position;
+	t_quat4f	*rotation;
+	t_vec3f		*orientation;
+	t_vec3f		*vup;
+	t_radius	*radius;
+	t_arrow		*arrow;
+	t_list		*next;
+}	t_focus;
+
+typedef t_vec3f	t_color3f;
+
 ssize_t		ft_pstderr(char const *str);
 void		ft_perror(char const *str);
 
 void		ft_tab_free(char **tab);
 size_t		ft_tab_size(char *const *tab);
 
-typedef t_vec3f	t_color3f;
-
 float		ft_str_to_float(char const *nptr);
 t_color3f	ft_str_to_color3f(char const *str);
 t_vec3f		ft_str_to_vec3f(char const *str);
 bool		ft_vec3f_isnormalized(t_vec3f const u);
+t_vec3f		ft_vec3f_rotate(t_vec3f const u, t_quat4f const q);
+
+t_radius	ft_radius(float const diameter);
+t_arrow		ft_arrow(t_point3f const center, \
+				t_vec3f const axis, float const height);
 
 /* ************************************************************************** */
 /*                                                                            */
@@ -102,8 +137,6 @@ typedef struct s_ambiance
 	float		lighting;
 	t_color3f	color;
 }	t_ambiance;
-
-typedef t_vec3f	t_point3f;
 
 typedef struct s_camera
 {
@@ -124,9 +157,7 @@ typedef struct s_light
 typedef struct s_sphere
 {
 	t_point3f	center;
-	float		radius;
-	float		radius_squared;
-	float		radius_reciprocal;
+	t_radius	radius;
 	t_color3f	color;
 }	t_sphere;
 
@@ -135,18 +166,16 @@ typedef struct s_plane
 	t_point3f	point;
 	t_vec3f		normal;
 	t_color3f	color;
+	t_quat4f	rotation;
 }	t_plane;
 
 typedef struct s_cylinder
 {
-	t_point3f	top;
-	t_point3f	base;
 	t_vec3f		axis;
-	float		radius;
-	float		radius_squared;
-	float		radius_reciprocal;
-	float		height;
+	t_radius	radius;
+	t_arrow		arrow;
 	t_color3f	color;
+	t_quat4f	rotation;
 }	t_cylinder;
 
 typedef struct s_scene
@@ -157,6 +186,7 @@ typedef struct s_scene
 	t_list		*spheres;
 	t_list		*planes;
 	t_list		*cylinders;
+	t_focus		focus;
 }	t_scene;
 
 bool		ft_ambiance_init(t_scene *const scene, char *const *info);
@@ -165,6 +195,13 @@ bool		ft_light_add(t_scene *const scene, char *const *info);
 bool		ft_sphere_add(t_scene *const scene, char *const *info);
 bool		ft_plane_add(t_scene *const scene, char *const *info);
 bool		ft_cylinder_add(t_scene *const scene, char *const *info);
+
+void		ft_focus_camera(t_scene *const scene);
+void		ft_focus_light(t_scene *const scene);
+void		ft_focus_sphere(t_scene *const scene);
+void		ft_focus_plane(t_scene *const scene);
+void		ft_focus_cylinder(t_scene *const scene);
+
 void		ft_scene_free(t_scene *scene);
 t_scene		*ft_scene_new(char const *file);
 
@@ -226,27 +263,20 @@ typedef struct s_hit
 	t_color3f	color;
 }	t_hit;
 
-typedef struct s_basis
-{
-	t_vec3f	x;
-	t_vec3f	y;
-	t_vec3f	z;
-}	t_basis;
-
 void		ft_xclient_free(t_xclient *xclient);
 t_xclient	*ft_xclient_new(t_scene *const scene);
 
-t_ray		ft_ray(t_point3f const origin, t_vec3f const direction);
 t_vec3f		ft_face_normal(t_vec3f const direction, t_vec3f const normal);
 void		ft_sphere_hit(t_sphere *const sp, t_ray const r, t_hit *const h);
 void		ft_plane_hit(t_plane *const pl, t_ray const r, t_hit *const h);
 void		ft_cylinder_hit(t_cylinder *const cy, \
 				t_ray const r, t_hit *const h);
-
 void		ft_pixel_put(t_xclient const *const xclient, \
 				int const x, int const y, t_color3f const color);
-void		ft_ray_tracing(t_xclient const *const xclient);
-t_vec3f		ft_vec3f_rotate(t_vec3f const u, t_quat4f const q);
+t_color3f	ft_ray_tracing(t_scene const *const scene, \
+				int const i, int const j);
+
+bool		ft_key_rotate(int const keycode, t_scene *const scene);
 int			ft_key_press(int const keycode, t_xclient *const xclient);
 
 #endif
