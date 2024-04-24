9efe3b0f5dca878c6d0056b68e72764fc66a05a3
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-24 14:49:48 +0200

    Updated ft_light_init

--- src/scene/ft_light_init.c
+++ src/scene/ft_light_init.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 12:48:21 by abenamar          #+#    #+#             */
-/*   Updated: 2024/04/24 04:01:14 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/04/24 13:40:55 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 bool	ft_light_init(t_scene *const scene, char *const *info)
 {
 	if (scene->light)
-		return (ft_pstderr(__ERR_08), false);
+		return (ft_pstderr(__ERR_07), false);
 	if (ft_tab_size(info) != 4)
-		return (ft_pstderr(__ERR_09), false);
+		return (ft_pstderr(__ERR_08), false);
 	scene->light = malloc(sizeof(t_light));
 	if (!scene->light)
-		return (ft_pstderr(__ERR_07), false);
+		return (ft_pstderr(__ERR_06), false);
 	scene->light->position = ft_str_to_vec3f(info[1]);
 	if (isnan(scene->light->position.x))
 		return (false);
@@ -28,7 +28,7 @@ bool	ft_light_init(t_scene *const scene, char *const *info)
 	if (!isfinite(scene->light->brightness))
 		return (ft_pstderr(__ERR_09), false);
 	if (signbit(scene->light->brightness) || scene->light->brightness > 1.0F)
-		return (ft_pstderr(__ERR_11), false);
+		return (ft_pstderr(__ERR_10), false);
 	scene->light->color = ft_str_to_color3f(info[3]);
 	if (isnan(scene->light->color.x))
 		return (false);
