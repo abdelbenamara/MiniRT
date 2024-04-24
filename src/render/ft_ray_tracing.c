794a08167750cea3a2b053d2957770a279c2a874
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-24 14:55:32 +0200

    Updated ft_ray_tracing

--- src/render/ft_ray_tracing.c
+++ src/render/ft_ray_tracing.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 00:23:38 by abenamar          #+#    #+#             */
-/*   Updated: 2024/04/22 16:32:14 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/04/24 14:03:42 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,63 @@ static t_viewport	ft_viewport(t_camera const *const camera)
 	return (viewport);
 }
 
+static bool
+	ft_scene_hit(t_scene const *const scene, \
+		t_point3f const origin, t_vec3f const direction, t_hit *const h)
+{
+	t_ray	r;
+	t_list	*lst;
+
+	r.origin = origin;
+	r.direction = direction;
+	h->t = INFINITY;
+	h->bias = _SHADOW_BIAS;
+	lst = scene->spheres;
+	while (lst)
+		(ft_sphere_hit(lst->content, &r, h), lst = lst->next);
+	lst = scene->planes;
+	while (lst)
+		(ft_plane_hit(lst->content, &r, h), lst = lst->next);
+	lst = scene->cylinders;
+	while (lst)
+		(ft_cylinder_hit(lst->content, &r, h), lst = lst->next);
+	if (!isfinite(h->t))
+		return (false);
+	return (true);
+}
+
+static t_color3f
+	ft_hit_color(t_scene const *const scene, \
+		t_point3f const origin, t_vec3f const direction)
+{
+	t_hit		h[2];
+	t_vec3f		dir;
+	float		d2cr[3];
+	t_color3f	ambient;
+
+	if (!ft_scene_hit(scene, origin, direction, h))
+		return (ft_vec3f(0.0F, 0.0F, 0.0F));
+	dir = ft_vec3f_diff(scene->light->position, h->point);
+	d2cr[0] = ft_vec3f_dot(dir, dir);
+	dir = ft_vec3f_unit(dir);
+	d2cr[1] = ft_vec3f_dot(dir, h->normal);
+	ambient = ft_vec3f(scene->ambiance->x * h->color.x, \
+		scene->ambiance->y * h->color.y, scene->ambiance->z * h->color.z);
+	if (signbit(d2cr[1]) || (ft_scene_hit(scene, h->point, dir, h + 1)
+			&& h[1].t * h[1].t < d2cr[0]))
+		return (ambient);
+	d2cr[2] = d2cr[1] * scene->light->brightness;
+	return (ft_vec3f_sum(ambient, \
+		ft_vec3f_prod(h->color, fmaxf(d2cr[2], d2cr[2] / d2cr[0]))));
+}
+
+static float	ft_random_float(float const min, float const max)
+{
+	static float const	rand_max_reciprocal = 1.0F / (RAND_MAX + 1.0F);
+
+	return (min + (max - min) * rand() * rand_max_reciprocal);
+}
+
 t_color3f	ft_ray_tracing(t_scene const *const scene, int const i, int const j)
 {
 	t_viewport const	vp = ft_viewport(scene->camera);
@@ -40,15 +97,14 @@ t_color3f	ft_ray_tracing(t_scene const *const scene, int const i, int const j)
 	t_color3f			color;
 
 	color = ft_hit_color(scene, origin, \
-		ft_vec3f_unit(ft_vec3f_diff(target, origin)), _MAX_DEPTH);
+		ft_vec3f_unit(ft_vec3f_diff(target, origin)));
 	k = 1;
 	while (k < _SAMPLES_PER_PIXEL)
 	{
 		color = ft_vec3f_sum(color, ft_hit_color(scene, origin, \
 			ft_vec3f_unit(ft_vec3f_diff(ft_vec3f_sum(target, \
 			ft_vec3f_sum(ft_vec3f_prod(vp.pdu, ft_random_float(-0.5F, 0.5F)), \
-			ft_vec3f_prod(vp.pdv, ft_random_float(-0.5F, 0.5F)))), \
-			origin)), _MAX_DEPTH));
+			ft_vec3f_prod(vp.pdv, ft_random_float(-0.5F, 0.5F)))), origin))));
 		++k;
 	}
 	return (color);
