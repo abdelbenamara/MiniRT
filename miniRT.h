9daaff8742a5c3240891ad9001f8e2a7f64d2f45
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-24 04:16:14 +0200

    Updated miniRT

--- miniRT.h
+++ miniRT.h
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 18:08:16 by abenamar          #+#    #+#             */
-/*   Updated: 2024/04/08 14:54:10 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/04/24 04:09:55 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,21 +62,26 @@ t_quat4f	ft_quat4f_unit(t_quat4f const q);
 /* ************************************************************************** */
 
 # define __USAGE	"Usage: miniRT file.rt\n"
-# define __ERR_1	"Error: wrong file extension (not *.rt)\n"
-# define __ERR_2	"Error: out of memory\n"
-# define __ERR_3	"Error: duplicated capital identifier (not unique A,C,L)\n"
-# define __ERR_4	"Error: wrong number of element informations\n"
-# define __ERR_5	"Error: wrong floating-point value (not finite number)\n"
-# define __ERR_6	"Error: invalid lightning ratio (not in range [0,1])\n"
-# define __ERR_7	"Error: wrong number of RGB colors (not exactly 3)\n"
-# define __ERR_8	"Error: invalid RGB value (not in range [0,255])\n"
-# define __ERR_9	"Error: wrong number of 3D coordinates (not exactly 3)\n"
-# define __ERR_10	"Error: invalid normalized value (not in range [-1,1])\n"
-# define __ERR_11	"Error: forbidden null vector (not normalized vector)\n"
-# define __ERR_12	"Error: invalid field of view (not in range [0,180])\n"
-# define __ERR_13	"Error: wrong element identifier (not A,C,L,sp,pl,cy)\n"
-# define __ERR_14	"Error: invalid distance (not greater or equal to 0)\n"
-# define __ERR_15	"Error: missing camera declaration (no 'C' identifier)\n"
+# define __ERR_01	"Error: invalid window width (not greater to 0)\n"
+# define __ERR_02	"Error: invalid window height (not greater to 0)\n"
+# define __ERR_03	"Error: invalid samples per pixel (not greater to 0)\n"
+# define __ERR_04	"Error: invalid max depth (not greater or equal to 0)\n"
+# define __ERR_05	"Error: invalid shadow bias (not greater or equal to 0)\n"
+# define __ERR_06	"Error: wrong file extension (not *.rt)\n"
+# define __ERR_07	"Error: out of memory\n"
+# define __ERR_08	"Error: duplicated capital identifier (not unique A,C,L)\n"
+# define __ERR_09	"Error: wrong number of element informations\n"
+# define __ERR_10	"Error: wrong floating-point value (not finite number)\n"
+# define __ERR_11	"Error: invalid lightning ratio (not in range [0,1])\n"
+# define __ERR_12	"Error: wrong number of RGB colors (not exactly 3)\n"
+# define __ERR_13	"Error: invalid RGB value (not in range [0,255])\n"
+# define __ERR_14	"Error: wrong number of 3D coordinates (not exactly 3)\n"
+# define __ERR_15	"Error: invalid normalized value (not in range [-1,1])\n"
+# define __ERR_16	"Error: forbidden null vector (not normalized vector)\n"
+# define __ERR_17	"Error: invalid field of view (not in range [0,180])\n"
+# define __ERR_18	"Error: wrong element identifier (not A,C,L,sp,pl,cy)\n"
+# define __ERR_19	"Error: invalid distance (not greater or equal to 0)\n"
+# define __ERR_20	"Error: missing capital identifier (not one of A,C,L)\n"
 
 typedef struct s_radius
 {
@@ -119,24 +124,18 @@ size_t		ft_tab_size(char *const *tab);
 float		ft_str_to_float(char const *nptr);
 t_color3f	ft_str_to_color3f(char const *str);
 t_vec3f		ft_str_to_vec3f(char const *str);
-bool		ft_vec3f_isnormalized(t_vec3f const u);
+bool		ft_vec3f_isnormalized(t_vec3f const *const u);
 t_vec3f		ft_vec3f_rotate(t_vec3f const u, t_quat4f const q);
-
 t_radius	ft_radius(float const diameter);
 t_arrow		ft_arrow(t_point3f const center, \
 				t_vec3f const axis, float const height);
+float		ft_random_float(float const min, float const max);
 
 /* ************************************************************************** */
 /*                                                                            */
 /*                                   scene                                    */
 /*                                                                            */
 /* ************************************************************************** */
-
-typedef struct s_ambiance
-{
-	float		lighting;
-	t_color3f	color;
-}	t_ambiance;
 
 typedef struct s_camera
 {
@@ -180,18 +179,17 @@ typedef struct s_cylinder
 
 typedef struct s_scene
 {
-	t_ambiance	*ambiance;
+	t_color3f	*ambiance;
 	t_camera	*camera;
-	t_list		*lights;
+	t_light		*light;
 	t_list		*spheres;
 	t_list		*planes;
 	t_list		*cylinders;
 	t_focus		focus;
 }	t_scene;
 
-bool		ft_ambiance_init(t_scene *const scene, char *const *info);
 bool		ft_camera_init(t_scene *const scene, char *const *info);
-bool		ft_light_add(t_scene *const scene, char *const *info);
+bool		ft_light_init(t_scene *const scene, char *const *info);
 bool		ft_sphere_add(t_scene *const scene, char *const *info);
 bool		ft_plane_add(t_scene *const scene, char *const *info);
 bool		ft_cylinder_add(t_scene *const scene, char *const *info);
@@ -212,19 +210,27 @@ t_scene		*ft_scene_new(char const *file);
 /* ************************************************************************** */
 
 # ifndef _WIDTH
-#  define _WIDTH	320
+#  define _WIDTH				640
 # endif
 
 # ifndef _HEIGHT
-#  define _HEIGHT	180
+#  define _HEIGHT				360
 # endif
 
 # ifndef _TITLE
-#  define _TITLE	"MiniRT"
+#  define _TITLE				"MiniRT"
 # endif
 
 # ifndef _SAMPLES_PER_PIXEL
 #  define _SAMPLES_PER_PIXEL	1
+# endif
+
+# ifndef _MAX_DEPTH
+#  define _MAX_DEPTH			1
+# endif
+
+# ifndef _SHADOW_BIAS
+#  define _SHADOW_BIAS			1.0E-4F
 # endif
 
 typedef struct s_xclient
@@ -267,14 +273,18 @@ void		ft_xclient_free(t_xclient *xclient);
 t_xclient	*ft_xclient_new(t_scene *const scene);
 
 t_vec3f		ft_face_normal(t_vec3f const direction, t_vec3f const normal);
-void		ft_sphere_hit(t_sphere *const sp, t_ray const r, t_hit *const h);
-void		ft_plane_hit(t_plane *const pl, t_ray const r, t_hit *const h);
-void		ft_cylinder_hit(t_cylinder *const cy, \
-				t_ray const r, t_hit *const h);
-void		ft_pixel_put(t_xclient const *const xclient, \
-				int const x, int const y, t_color3f const color);
+void		ft_sphere_hit(t_sphere const *const sp, \
+				t_ray const *const r, t_hit *const h);
+void		ft_plane_hit(t_plane const *const pl, \
+				t_ray const *const r, t_hit *const h);
+void		ft_cylinder_hit(t_cylinder const *const cy, \
+				t_ray const *const r, t_hit *const h);
+t_color3f	ft_hit_color(t_scene const *const scene, t_point3f const origin, \
+				t_vec3f const direction, int const depth);
 t_color3f	ft_ray_tracing(t_scene const *const scene, \
 				int const i, int const j);
+void		ft_pixel_put(t_xclient const *const xclient, \
+				int const x, int const y, t_color3f const color);
 
 bool		ft_key_rotate(int const keycode, t_scene *const scene);
 int			ft_key_press(int const keycode, t_xclient *const xclient);
