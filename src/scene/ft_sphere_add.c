6092b3506fd6e364607c8f7376e5b63a07abb08a
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-24 04:21:45 +0200

    Updated ft_sphere_add

--- src/scene/ft_sphere_add.c
+++ src/scene/ft_sphere_add.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 15:34:18 by abenamar          #+#    #+#             */
-/*   Updated: 2024/04/06 17:57:30 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/04/24 04:01:14 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,21 @@ bool	ft_sphere_add(t_scene *const scene, char *const *info)
 	t_list		*new;
 
 	if (ft_tab_size(info) != 4)
-		return (ft_pstderr(__ERR_4), false);
+		return (ft_pstderr(__ERR_09), false);
 	sp = malloc(sizeof(t_sphere));
+	if (!sp)
+		return (ft_pstderr(__ERR_07), false);
 	new = ft_lstnew(sp);
-	if (!sp || !new)
-		return (ft_pstderr(__ERR_2), free(sp), free(new), false);
+	if (!new)
+		return (ft_pstderr(__ERR_07), free(sp), false);
 	sp->center = ft_str_to_vec3f(info[1]);
 	if (isnan(sp->center.x))
 		return (ft_lstdelone(new, free), false);
 	sp->radius.twice = ft_str_to_float(info[2]);
 	if (!isfinite(sp->radius.twice))
-		return (ft_pstderr(__ERR_5), ft_lstdelone(new, free), false);
+		return (ft_pstderr(__ERR_09), ft_lstdelone(new, free), false);
 	if (signbit(sp->radius.twice))
-		return (ft_pstderr(__ERR_14), ft_lstdelone(new, free), false);
+		return (ft_pstderr(__ERR_19), ft_lstdelone(new, free), false);
 	sp->color = ft_str_to_color3f(info[3]);
 	if (isnan(sp->color.x))
 		return (ft_lstdelone(new, free), false);
