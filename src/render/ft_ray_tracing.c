bdf1cea1c9d6b76b0c3725a6f4558fae19f6ad7c
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-24 04:24:59 +0200

    Updated ft_ray_tracing

--- src/render/ft_ray_tracing.c
+++ src/render/ft_ray_tracing.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 00:23:38 by abenamar          #+#    #+#             */
-/*   Updated: 2024/04/03 02:21:23 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/04/22 16:32:14 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,54 +30,25 @@ static t_viewport	ft_viewport(t_camera const *const camera)
 	return (viewport);
 }
 
-static t_ray	ft_ray(t_point3f const origin, t_vec3f const direction)
-{
-	t_ray	r;
-
-	r.origin = origin;
-	r.direction = direction;
-	return (r);
-}
-
-static t_color3f	ft_hit_color(t_scene const *const scene, t_ray const r)
-{
-	t_hit	h;
-	t_list	*lst;
-
-	h.t = INFINITY;
-	h.color = ft_vec3f(0.0F, 0.0F, 0.0F);
-	lst = scene->spheres;
-	while (lst)
-		(ft_sphere_hit(lst->content, r, &h), lst = lst->next);
-	lst = scene->planes;
-	while (lst)
-		(ft_plane_hit(lst->content, r, &h), lst = lst->next);
-	lst = scene->cylinders;
-	while (lst)
-		(ft_cylinder_hit(lst->content, r, &h), lst = lst->next);
-	return (h.color);
-}
-
 t_color3f	ft_ray_tracing(t_scene const *const scene, int const i, int const j)
 {
 	t_viewport const	vp = ft_viewport(scene->camera);
-	t_point3f const		point = ft_vec3f_sum(vp.p00, ft_vec3f_sum(\
-		ft_vec3f_prod(vp.pdu, i), ft_vec3f_prod(vp.pdv, j)));
+	t_point3f const		target = ft_vec3f_sum(vp.p00, \
+		ft_vec3f_sum(ft_vec3f_prod(vp.pdu, i), ft_vec3f_prod(vp.pdv, j)));
 	t_vec3f const		origin = scene->camera->position;
 	int					k;
 	t_color3f			color;
 
-	color = ft_hit_color(\
-		scene, ft_ray(origin, ft_vec3f_unit(ft_vec3f_diff(point, origin))));
+	color = ft_hit_color(scene, origin, \
+		ft_vec3f_unit(ft_vec3f_diff(target, origin)), _MAX_DEPTH);
 	k = 1;
 	while (k < _SAMPLES_PER_PIXEL)
 	{
-		color = ft_vec3f_sum(color, ft_hit_color(scene, \
-			ft_ray(origin, ft_vec3f_unit(ft_vec3f_diff(\
-			ft_vec3f_sum(point, ft_vec3f_sum(\
-			ft_vec3f_prod(vp.pdu, -0.5F * rand() / (RAND_MAX + 1.0F)), \
-			ft_vec3f_prod(vp.pdv, -0.5F * rand() / (RAND_MAX + 1.0F)))), \
-			origin)))));
+		color = ft_vec3f_sum(color, ft_hit_color(scene, origin, \
+			ft_vec3f_unit(ft_vec3f_diff(ft_vec3f_sum(target, \
+			ft_vec3f_sum(ft_vec3f_prod(vp.pdu, ft_random_float(-0.5F, 0.5F)), \
+			ft_vec3f_prod(vp.pdv, ft_random_float(-0.5F, 0.5F)))), \
+			origin)), _MAX_DEPTH));
 		++k;
 	}
 	return (color);
