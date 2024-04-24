f38186c4525385a22ec91e384018dd5264022158
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-24 14:48:03 +0200

    Updated ft_vec3f_isnormalized

--- src/utils/ft_vec3f_isnormalized.c
+++ src/utils/ft_vec3f_isnormalized.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 14:41:11 by abenamar          #+#    #+#             */
-/*   Updated: 2024/04/23 20:48:55 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/04/24 13:44:51 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ bool	ft_vec3f_isnormalized(t_vec3f const *const u)
 {
 	if (u->x < -1.0F || u->y < -1.0F || u->z < -1.0F
 		|| u->x > 1.0F || u->y > 1.0F || u->z > 1.0F)
-		return (ft_pstderr(__ERR_15), false);
+		return (ft_pstderr(__ERR_14), false);
 	if (fpclassify(u->x) == FP_ZERO
 		&& fpclassify(u->y) == FP_ZERO
 		&& fpclassify(u->z) == FP_ZERO)
-		return (ft_pstderr(__ERR_16), false);
+		return (ft_pstderr(__ERR_15), false);
 	return (true);
 }
