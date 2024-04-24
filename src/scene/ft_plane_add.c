31d9955acfd45e8dd5027658f8fd8f0e2cc08731
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-24 04:21:48 +0200

    Updated ft_plane_add

--- src/scene/ft_plane_add.c
+++ src/scene/ft_plane_add.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 15:35:10 by abenamar          #+#    #+#             */
-/*   Updated: 2024/04/04 01:08:01 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/04/24 04:01:14 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,16 +29,18 @@ bool	ft_plane_add(t_scene *const scene, char *const *info)
 	t_list	*new;
 
 	if (ft_tab_size(info) != 4)
-		return (ft_pstderr(__ERR_4), false);
+		return (ft_pstderr(__ERR_09), false);
 	pl = malloc(sizeof(t_plane));
+	if (!pl)
+		return (ft_pstderr(__ERR_07), false);
 	new = ft_lstnew(pl);
-	if (!pl || !new)
-		return (ft_pstderr(__ERR_2), free(pl), free(new), false);
+	if (!new)
+		return (ft_pstderr(__ERR_07), free(pl), false);
 	pl->point = ft_str_to_vec3f(info[1]);
 	if (isnan(pl->point.x))
 		return (ft_lstdelone(new, free), false);
 	pl->normal = ft_str_to_vec3f(info[2]);
-	if (isnan(pl->normal.x) || !ft_vec3f_isnormalized(pl->normal))
+	if (isnan(pl->normal.x) || !ft_vec3f_isnormalized(&pl->normal))
 		return (ft_lstdelone(new, free), false);
 	pl->color = ft_str_to_color3f(info[3]);
 	if (isnan(pl->color.x))
