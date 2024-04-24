cba8a7a231cb6619a7990a1dab952314a33b789a
Abdel Benamara <abdelb.contact@gmail.com>
2024-04-24 04:22:22 +0200

    Updated ft_scene_free

--- src/scene/ft_scene_free.c
+++ src/scene/ft_scene_free.c
@@ -6,7 +6,7 @@
 /*   By: abenamar <abenamar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 11:41:04 by abenamar          #+#    #+#             */
-/*   Updated: 2024/01/05 15:17:09 by abenamar         ###   ########.fr       */
+/*   Updated: 2024/04/23 19:41:59 by abenamar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_scene_free(t_scene *scene)
 {
 	free(scene->ambiance);
 	free(scene->camera);
-	ft_lstclear(&scene->lights, free);
+	free(scene->light);
 	ft_lstclear(&scene->spheres, free);
 	ft_lstclear(&scene->planes, free);
 	ft_lstclear(&scene->cylinders, free);
